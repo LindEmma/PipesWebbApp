935721917c4a8c5ad4cbc2b8d763b6cd7129f3ff
Emma <elune.lind@gmail.com>
2024-09-16 13:19:58 +0200

    Add project files.

---
+++ PipesWebbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PipesWebbApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
